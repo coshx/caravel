3bbf7add70e16ecb7f87e1f3bef8c9f31f62b633
Adrien Cadet <acadet@live.fr>
2015-05-28 19:20:26 -0700

    Adds test project

---
+++ caravel-test/caravel-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  caravel-test
+//
+//  Created by Adrien on 28/05/15.
+//  Copyright (c) 2015 Coshx Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
