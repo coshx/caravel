ed3f7cb10dbcc60496acbc8c7d24839a88624649
Adrien Cadet <acadet@live.fr>
2015-06-19 15:22:54 -0700

    Adds missing subscript extension

--- caravel/utils/StringExtension.swift
+++ caravel/utils/StringExtension.swift
@@ -8,12 +8,16 @@
 
 import Foundation
 
-extension String {
+internal extension String {
     subscript (i: Int) -> Character {
         return self[advance(self.startIndex, i)]
     }
     
     subscript (i: Int) -> String {
         return String(self[i] as Character)
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        return substringWithRange(Range(start: advance(startIndex, r.startIndex), end: advance(startIndex, r.endIndex)))
     }
 }
